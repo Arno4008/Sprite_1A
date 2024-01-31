5af112c5f5253e5fd3740c97ac7d9c5b0f3db3a9
unknown <alwena>
2024-01-31 15:58:38 +0100

    PressE

--- Assets/Scripts/Items/PilierController.cs
+++ Assets/Scripts/Items/PilierController.cs
@@ -6,25 +6,30 @@ public class PilierController : MonoBehaviour
 {
     public GameObject DropZone;
     public bool isEmpty = true;
+    public GameObject PressE;
+    public Inventory Inventory;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player.GetComponent<Inventory>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
-    { 
-        if(other.gameObject.CompareTag("Player") && isEmpty)
+    {
+        if (other.gameObject.CompareTag("Player") && isEmpty && Inventory.inventory.Count > 0)
         {
             // can press E to drop item
             other.gameObject.GetComponent<Player>().canDropItem = true;
             other.gameObject.GetComponent<Player>().DropZone = DropZone;
+            PressE.SetActive(true);
         }
     }
 
@@ -35,6 +40,7 @@ public class PilierController : MonoBehaviour
             // can press E to drop item
             other.gameObject.GetComponent<Player>().canDropItem = false;
             other.gameObject.GetComponent<Player>().DropZone = null;
+            PressE.SetActive(false);
         }
     }
 }
